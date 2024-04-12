59ab23fd49127d556bed5e87adc1fb1c2228bca0
Leonardo Taquini <leonardogtaquini@gmail.com>
2024-04-12 10:43:39 -0300

    minimo funcional ( nunca hice un codigo tan asqueroso )

--- __init__.py
+++
@@ -1,1 +0,0 @@
-from main import conectar_mysql, load_dotenv
--- main.py
+++ main.py
@@ -22,7 +22,6 @@ def verificar_y_eliminar_tabla(cursor_param, tabla_param):
         
         if existe_tabla:
             cursor_param.execute(f"DROP TABLE {tabla_param}")
-            print(f"La tabla '{tabla_param}' ha sido eliminada correctamente")
     except Exception as e:
         print(f"Error al verificar y eliminar la tabla: {e}")
 
@@ -45,6 +44,54 @@ def cargar_datos_desde_csv(cursor_param, archivo_csv_param, tabla_param):
             cursor_param.execute(consulta, fila)
 
 
+def listar_localidades(cursor_param):
+    cursor_param.execute("SELECT * FROM localidades")
+    resultado = cursor_param.fetchall()
+    return resultado         
+
+
+def agrupar_provincias_con_id(lista_localidades):
+    provincias = {}
+    for localidad in lista_localidades:
+        provincia = localidad[0].lower()
+        if provincia not in provincias:
+            provincias[ localidad[0] ] = localidad[4]
+    return provincias     
+
+def remover_repetidos( lista ):
+    finally_dict = {}
+    for item, val in lista.items():
+        finally_dict[ item ] = set( val )
+    return finally_dict        
+
+
+def agrupar_provincia_localidad( localidades_arg ):
+    provincias = agrupar_provincias_con_id( localidades_arg )
+    provincia_por_localidad = {}
+    for localidad in localidades_arg:
+        id_prov = localidad[4]
+        nombre_prov = localidad[0]
+        nombre_localidad = localidad[2]
+
+        if id_prov == provincias.get( nombre_prov ):
+            if nombre_prov not in provincia_por_localidad:
+                provincia_por_localidad[ nombre_prov ] = [ nombre_localidad ]
+            else:
+                provincia_por_localidad[ nombre_prov ].append( nombre_localidad )
+    provincia_por_localidad = remover_repetidos( provincia_por_localidad )
+    return provincia_por_localidad
+
+
+carpeta_provincia = 'provincias'
+if not os.path.exists(carpeta_provincia):
+    os.makedirs(carpeta_provincia)
+
+def escribir_csv_provincia(provincia, localidades):
+    nombre_archivo = os.path.join(carpeta_provincia, f'{provincia}.csv')
+    with open(nombre_archivo, 'w', newline='', encoding='utf-8') as archivo_csv:
+        escritor_csv = csv.writer(archivo_csv)
+        escritor_csv.writerow(['Localidades de', provincia])
+        escritor_csv.writerows(localidades)
 
 host = os.environ["DB_HOST"]
 user = os.environ["DB_USERNAME"] 
@@ -63,6 +110,16 @@ verificar_y_eliminar_tabla(cursor, tabla)
 crear_tabla_desde_csv(cursor, archivo_csv, tabla)
 
 cargar_datos_desde_csv(cursor, archivo_csv, tabla)
+
+localidades = listar_localidades(cursor)
+
+
+provincias = agrupar_provincia_localidad(localidades)
+
+
+for provincia, localidades in provincias.items():
+    escribir_csv_provincia(provincia, localidades)
+
 
 conn.commit()
 
